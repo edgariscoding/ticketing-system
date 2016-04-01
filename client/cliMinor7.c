2e9fd50988c0b3fa16722f0fd1f084f29859ed22
Edgar Sanchez <edgr.sanchez@outlook.com>
2016-04-01 15:10:41 -0500

    Initial commit

---
+++ client/cliMinor7.c
@@ -0,0 +1,22 @@
+// =====================================================================
+// Name:            Edgar Sanchez
+// Course:          CSCE 3600
+// Date:            April 11, 2016
+// Title:           Minor Assignment 7 Client
+// Version:         1.0
+// Description:     This program will consist of a “main” ticket outlet
+//                  (i.e., the server) that will provide services to
+//                  “BUY” and “RETURN” tickets to two “local” ticket
+//                  distributors (i.e., the clients).
+// Format:          client
+// =====================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char* argv[]) {
+
+	printf("I am a client!\n");
+	return EXIT_SUCCESS;
+}
---
+++ server/svrMinor7.c
@@ -0,0 +1,22 @@
+// =====================================================================
+// Name:            Edgar Sanchez
+// Course:          CSCE 3600
+// Date:            April 11, 2016
+// Title:           Minor Assignment 7 Server
+// Version:         1.0
+// Description:     This program will consist of a “main” ticket outlet
+//                  (i.e., the server) that will provide services to
+//                  “BUY” and “RETURN” tickets to two “local” ticket
+//                  distributors (i.e., the clients).
+// Format:          server
+// =====================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char* argv[]) {
+
+	printf("I am a client!\n");
+	return EXIT_SUCCESS;
+}
